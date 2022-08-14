bd65cd4d2ef820faa71a0b18d656af7a6e19ebc4
apaudel96 <apaudel96@ymail.com>
2022-08-14 19:31:50 +0100

    Update response codes

--- server/app/middlewares/authentication.py
+++ server/app/middlewares/authentication.py
@@ -1,38 +1,3 @@
-# import typing
-# from starlette.requests import HTTPConnection
-# from starlette.authentication import AuthenticationBackend, AuthCredentials, BaseUser
-# from app.models.users import Token, User
-
-
-# class TokenAuthentication(AuthenticationBackend):
-#     async def authenticate(
-#         self, conn: HTTPConnection
-#     ) -> typing.Optional[typing.Tuple["AuthCredentials", "BaseUser"]]:
-#         # check if authorization header is present
-#         authorization = conn.headers.get("Authorization")
-#         if not authorization:
-#             return
-#         # check if bearer token is present
-#         if not authorization.startswith("Bearer "):
-#             return
-
-#         # get token_content
-#         token_content = authorization.split(" ")[1]
-
-#         # get token from db
-#         token = await Token.filter(token=token_content).first()
-#         if not token:
-#             return
-#         # check if token is expired
-#         if token.is_expired:
-#             return
-#         # get user from db
-#         user = await User.filter(id=token.user_id).first()
-#         if not user:
-#             return
-#         # return AuthCredentials and BaseUser
-#         return AuthCredentials(["authenticated"]), user
-
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from app.models.users import Token, User
--- server/app/routes/tasks.py
+++ server/app/routes/tasks.py
@@ -1,29 +1,30 @@
 from fastapi.routing import APIRouter
 from fastapi.responses import JSONResponse
-from fastapi.requests import Request
 from app.models.tasks import Task, TaskCreateInput, TaskOutput, TaskUpdateInput
 from ..models.users import User
 from app.middlewares.authentication import get_current_user
 from fastapi import Depends
+
 router = APIRouter(prefix="/tasks", tags=["Task"])
 
 # task list
-@router.get("/", response_model=list[TaskOutput])
-async def list_tasks(user:User = Depends(get_current_user)):
+@router.get("/", response_model=list[TaskOutput], responses={401: {}})
+async def list_tasks(user: User = Depends(get_current_user)):
     if user:
-        tasks = await Task.filter(user = user).all()
+        tasks = await Task.filter(user=user).all()
         return tasks
 
+
 # task create
-@router.post("/", response_model=TaskOutput, status_code=201)
-async def create_task(data: TaskCreateInput, user:User = Depends(get_current_user)):
+@router.post("/", response_model=TaskOutput, status_code=201, responses={401: {}})
+async def create_task(data: TaskCreateInput, user: User = Depends(get_current_user)):
     task = await Task.create(user=user, **data.dict(exclude_unset=True))
     return task
 
 
 # task detail
-@router.get("/{id}", response_model=TaskOutput, responses={404: {}})
-async def get_task(request: Request, id: int, user:User = Depends(get_current_user)):
+@router.get("/{id}", response_model=TaskOutput, responses={404: {}, 401: {}})
+async def get_task(id: int, user: User = Depends(get_current_user)):
     task = await Task.filter(user=user, id=id).first()
     if not task:
         return JSONResponse(status_code=404, content={"detail": "Task not found"})
@@ -31,8 +32,10 @@ async def get_task(request: Request, id: int, user:User = Depends(get_current_us
 
 
 # task update
-@router.put("/{id}", response_model=TaskOutput, responses={404: {}})
-async def update_task(request: Request, id: int, data: TaskUpdateInput, user:User = Depends(get_current_user)):
+@router.put("/{id}", response_model=TaskOutput, responses={404: {}, 401: {}})
+async def update_task(
+    id: int, data: TaskUpdateInput, user: User = Depends(get_current_user)
+):
     task = await Task.filter(id=id, user=user).first()
     if not task:
         return JSONResponse(status_code=404, content={"detail": "Task not found"})
@@ -42,8 +45,8 @@ async def update_task(request: Request, id: int, data: TaskUpdateInput, user:Use
 
 
 # task delete
-@router.delete("/{id}", status_code=204, responses={404: {}})
-async def delete_task(request: Request, id: int, user:User = Depends(get_current_user)):
+@router.delete("/{id}", status_code=204, responses={404: {}, 401: {}})
+async def delete_task(id: int, user: User = Depends(get_current_user)):
     task = await Task.filter(id=id, user=user).first()
     if not task:
         return JSONResponse(status_code=404, content={"detail": "Task not found"})
